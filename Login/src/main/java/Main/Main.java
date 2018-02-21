5f89d41fa9796f7800c9f96e5e4d4c58ea85e008
李玥 <lkjx3031274@163.com>
2018-02-21 22:36:34 +0800

    update

--- Login/src/main/java/Main/Main.java
+++
@@ -1,9 +0,0 @@
-package Main;
-
-import common.Out;
-
-public class Main {
-	public static void main(String [] args){
-        Out.info(1);
-    }
-}
---
+++ Login/src/main/java/main/Main.java
@@ -0,0 +1,9 @@
+package main;
+
+import common.Out;
+
+public class Main {
+	public static void main(String [] args){
+        Out.info(1);
+    }
+}
